d893c4f4dda875b15ece058fac1b603300aedacb
Bilal1611 <mbi.artificial.intelligence.com>
2020-01-20 12:24:08 +0100

    Aktualisiert dasdf

---
+++ Kurs_9/GitHubAk.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd KI_Kurse/
+git add Kurs_9/
+git add test1.sh
+git commit -m "Aktualisiert dasdf"
+git push origin master
+
+#$git config credential.helper store
+#$git push ssh://git@github.com/username/repo.git
+
+#Username for 'https://github.com': <USERNAME>
+#Password for 'https://USERNAME@github.com': <PASSWORD>
+
+exec bash
+

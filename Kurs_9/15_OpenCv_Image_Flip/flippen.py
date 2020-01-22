5fee38ae1a26cbb09b449fe4a9c41b634d96a46c
Bilal1611 <mbi.artificial.intelligence.com>
2020-01-22 17:02:14 +0100

    Aktualisiert dasdf

--- Kurs_9/15_OpenCv_Image_Flip/flippen.py
+++ Kurs_9/15_OpenCv_Image_Flip/flippen.py
@@ -1,20 +1,23 @@
 import numpy as np 
 import argparse 
-import resize 
+import flip 
 import cv2 
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required = True, help = "Enter Path to the Image")
 args = vars(ap.parse_args())
-
+# Bild hochladen
 image = cv2.imread(args["image"])
-
 cv2.imshow("Orginal", image)
 
-resized1 = resize.resizing(image, width = 100)
-cv2.imshow("Groesse um 100 veraendert", resized1)
 
-resized = resize.resizing(image, height = 100)
-cv2.imshow("Hoehe um 10 veraendert", resized)
+flipped = cv2.flip(image, 0)
+cv2.imshow("Vertikal 0", flipped)
+
+flipped = cv2.flip(image, 1)
+cv2.imshow("Horizental 1", flipped)
+
+flipped = cv2.flip(image, -1)
+cv2.imshow("Beide Flippen -1", flipped)
 
 cv2.waitKey(0)
